0390254596fce53d098b515d11552d6dd9245618
Chetan Shintre <chetanshintre2@gmail.com>
2023-10-21 10:56:16 +0530

    added get route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  hello(){
+    return { "message":"Website is up and running" };
+  }
 
   @Post('/dump')
   async dump(){
